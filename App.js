b71fc467af5c4ea6f537f272859b803ee3e751d3
sebastianoscarlopez <sebastianoscarlopez@gmail.com>
2018-03-21 17:02:37 -0300

    Saved Work

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 
-import store from './app/store'; //Import the store
-import Home from './app/components/home' //Import the component file
+import store from './app/config/store'; //Import the store
+//import Home from './app/screens/home' //Import the component file
+import Login from './app/screens/login' //Import the component file
 
 export default class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <Home />
+                <Login />
             </Provider>
         );
     }
